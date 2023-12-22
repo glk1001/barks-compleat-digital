d5c7fd33a10a83bb1954f4bfbbf3aafc19bec371
glk1001 <gregg.kay@gmail.com>
2023-12-22 12:56:42 +1100

    Added 'The Fabulous Philosopher's Stone', 'The Great Steamboat Race', and 'Riches, Riches, Everywhere\!'.

--- comics_info.py
+++ comics_info.py
@@ -240,6 +240,9 @@ SERIES_INFO: Dict[str, SeriesInfo] = {
     "The Seven Cities of Cibola": SeriesInfo(RTOM, SERIES_US),
     "The Mysterious Stone Ray": SeriesInfo(RTOM, SERIES_US),
     "The Lemming with the Locket": SeriesInfo(RTOM, SERIES_US),
+    "The Fabulous Philosopher's Stone": SeriesInfo(RTOM, SERIES_US),
+    "The Great Steamboat Race": SeriesInfo(RTOM, SERIES_US),
+    "Riches, Riches, Everywhere!": SeriesInfo(RTOM, SERIES_US),
 
     "Managing the Echo System": SeriesInfo(RTOM, SERIES_CS),
     "Plenty of Pets": SeriesInfo(RTOM, SERIES_CS),
