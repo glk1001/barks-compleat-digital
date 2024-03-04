a6466e8d02b64afc17e3df770bb76c4cd9229c45
glk1001 <gregg.kay@gmail.com>
2024-03-04 11:40:13 +1100

    Put back external median.

--- remove_alias_artifacts.py
+++ remove_alias_artifacts.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import time
 
 import cv2 as cv
@@ -7,10 +8,38 @@ from numba import jit
 
 DEBUG = False
 DEBUG_OUTPUT_DIR = "/tmp"
+TEMP_DIR = "/tmp"
 
 MEDIAN_BLUR_APERTURE_SIZE = 7
 ADAPTIVE_THRESHOLD_BLOCK_SIZE = 21
 ADAPTIVE_THRESHOLD_CONST_SUBTRACT = 10
+
+
+def median_filter_external(original_image, mask_image, kernel_size: int):
+    image_file = os.path.join(TEMP_DIR, "src-image.jpg")
+    cv.imwrite(image_file, original_image)
+    mask_file = os.path.join(TEMP_DIR, "enlarged-black-ink-mask.jpg")
+    cv.imwrite(mask_file, mask_image)
+
+    masked_filter_exe = "/home/greg/Prj/github/opencv/test/build/MaskedMedian"
+    dest_file = os.path.join(TEMP_DIR, "dest.jpg")
+    run_args = [masked_filter_exe, image_file, mask_file, str(kernel_size), dest_file]
+    # print(run_args)
+
+    result = subprocess.run(
+        run_args,
+        shell=False,
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+    print(result.stdout)
+    print(result.stderr)
+
+    if result.returncode != 0:
+        raise Exception("Could not run masked median filter.")
+
+    return cv.imread(dest_file)
 
 
 def median_filter(original_image, mask, kernel_size: int):
@@ -46,6 +75,7 @@ def median_filter_core(wrapped_image, wrapped_mask, kernel_size: int, filtered_i
             for x in range(i - w, i + w + 1):
                 for y in range(j - w, j + w + 1):
                     if wrapped_mask[x, y] > 0:
+                        assert wrapped_mask[x, y] == 255
                         continue
                     pixel = wrapped_image[x, y]
                     nbrs0[num_nbrs] = pixel[0]
@@ -99,6 +129,9 @@ def remove_alias_artifacts(input_image):
     filtered_image = median_filter(
         input_image, enlarged_black_ink_mask, MEDIAN_BLUR_APERTURE_SIZE
     )
+    # filtered_image = median_filter_external(
+    #     input_image, enlarged_black_ink_mask, MEDIAN_BLUR_APERTURE_SIZE
+    # )
     if DEBUG:
         cv.imwrite(os.path.join(DEBUG_OUTPUT_DIR, "filtered-image.jpg"), filtered_image)
 
