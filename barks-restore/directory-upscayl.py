87b27c5ded7a2f20cb9fcd0361225cbb2f928bff
glk1001 <gregg.kay@gmail.com>
2025-02-03 14:42:33 +1100

    Added directory upscayl command.

---
+++ barks-restore/directory-upscayl.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from src.upscale_image import upscale_image_file
+
+if __name__ == "__main__":
+    scale = 4
+
+    input_image_dir = sys.argv[1]
+    output_image_dir = sys.argv[2]
+
+    if not os.path.isdir(input_image_dir):
+        print(f'ERROR: Can\'t find input directory: "{input_image_dir}".')
+        sys.exit(1)
+    if not os.path.isdir(output_image_dir):
+        print(f'WARN: Created new output directory: "{output_image_dir}".')
+        os.makedirs(output_image_dir)
+
+    for in_filename in os.listdir(input_image_dir):
+        in_file = os.path.join(input_image_dir, in_filename)
+        if not os.path.isfile(in_file):
+            continue
+
+        out_file = os.path.join(output_image_dir, in_filename)
+        if os.path.exists(out_file):
+            print(f'WARN: Target file exists - skipping: "{out_file}".')
+            continue
+        # if os.path.exists(out_file):
+        #     print(f'ERROR: Can\'t overwrite target file: "{out_file}".')
+        #     sys.exit(1)
+
+        upscale_image_file(in_file, out_file, scale)
