beca1f8e4e312e98c2ae1441db9602ed3ccb5b8d
glk1001 <gregg.kay@gmail.com>
2025-04-28 15:28:24 +1000

    Getting alignment right.

--- barks-reader/kivy-reader.py
+++ barks-reader/kivy-reader.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from dataclasses import dataclass
 from typing import List, Tuple
 
 from kivy.app import App
@@ -12,6 +13,33 @@ from kivy.utils import escape_markup
 from barks_fantagraphics.comics_cmd_args import CmdArgs, CmdArgNames
 from barks_fantagraphics.comics_utils import setup_logging
 from mcomix_reader import ComicReader
+
+
+@dataclass
+class ComicTitleInfo:
+    chronological_number: int
+    title: str
+    issue_title: str
+    filename: str
+
+
+def get_all_comic_titles(titles: List[str]) -> Tuple[List[ComicTitleInfo], int]:
+    titles_with_issue_nums = []
+    max_title_len = 0
+    for title in titles:
+        comic_book = comics_database.get_comic_book(title)
+        title_with_issue_num = comic_book.get_title_with_issue_num()
+        max_title_len = max(max_title_len, len(title_with_issue_num))
+        titles_with_issue_nums.append(
+            ComicTitleInfo(
+                comic_book.chronological_number,
+                title,
+                comic_book.get_comic_issue_title(),
+                title_with_issue_num,
+            )
+        )
+
+    return titles_with_issue_nums, max_title_len
 
 
 class ScrollableLabelList(ScrollView):
@@ -27,33 +55,59 @@ class ScrollableLabelList(ScrollView):
         self.bar_inactive_color = (0.8, 0.8, 0.8, 0.8)
         self.bar_width = 5
 
-        self.layout = GridLayout(cols=1, spacing=5, pos=(0, 0), size_hint_x=0.5, size_hint_y=None)
+        self.layout = GridLayout(
+            cols=3, padding=10, spacing=[15, 20], pos=(0, 0), size_hint_x=0.5, size_hint_y=None
+        )
         self.add_widget(self.layout)
         self.layout.bind(minimum_height=self.layout.setter("height"))
 
         self.comic_reader = ComicReader()
 
-    def add_item(self, text):
-        esc_text = escape_markup(text)
-        label = Label(
-            text=f"[ref={esc_text}]{text}[/ref]",
-            size_hint_y=None,
-            height=20,
+    def add_item(self, num: int, title: str, issue_title: str, comic_filename: str):
+        num_label = Label(
+            text=f"{num}",
+            size_hint=(None, 1.0),
+            width=35,
+            markup=False,
+            underline=False,
+            halign="right",
+            valign="middle",
+        )
+        num_label.bind(size=num_label.setter("text_size"))
+        self.layout.add_widget(num_label)
+
+        esc_filename = escape_markup(comic_filename)
+        text_label = Label(
+            text=f"[ref={esc_filename}]{title}[/ref]",
+            size_hint=(None, 1.0),
+            width=300,
             markup=True,
             underline=False,
             halign="left",
             valign="middle",
         )
-        label.bind(size=label.setter("text_size"))
-        label.bind(on_ref_press=self.comic_reader.show_comic)
-        self.layout.add_widget(label)
+        text_label.bind(on_ref_press=self.comic_reader.show_comic)
+        text_label.bind(size=text_label.setter("text_size"))
+        self.layout.add_widget(text_label)
+
+        issue_label = Label(
+            text=f"[{issue_title}]",
+            size_hint=(None, 1.0),
+            width=100,
+            markup=False,
+            underline=False,
+            halign="left",
+            valign="middle",
+        )
+        issue_label.bind(size=issue_label.setter("text_size"))
+        self.layout.add_widget(issue_label)
 
     def on_request_close(self):
         return self.comic_reader.on_app_request_close()
 
 
 class MyApp(App):
-    def __init__(self, title_with_issues: List[Tuple[int, str, str]], max_title_len: int, **kwargs):
+    def __init__(self, title_with_issues: List[ComicTitleInfo], max_title_len: int, **kwargs):
         super().__init__(**kwargs)
 
         self.title_with_issues = title_with_issues
@@ -64,25 +118,13 @@ class MyApp(App):
         self.title = "The Compleat Barks Reader"
 
         label_list = ScrollableLabelList()
-        for chronological_num, title, issue_title in self.title_with_issues:
-            label_list.add_item(f"{chronological_num:3} {title} {issue_title}")
+        for title_info in self.title_with_issues:
+            label_list.add_item(title_info.chronological_number, title_info.title,
+                                title_info.issue_title, title_info.filename)
         return label_list
 
     def on_request_close_window(self, *args):
         return self.root.on_request_close()
-
-
-def get_all_comic_titles(titles: List[str]) -> Tuple[List[Tuple[int, str, str]], int]:
-    titles_with_issue_nums = []
-    max_title_len = 0
-    for title in titles:
-        comic_book = comics_database.get_comic_book(title)
-        title_with_issue_num = comic_book.get_title_with_issue_num()
-        max_title_len = max(max_title_len, len(title_with_issue_num))
-        titles_with_issue_nums.append(
-                (comic_book.chronological_number, title, comic_book.get_comic_issue_title()))
-
-    return titles_with_issue_nums, max_title_len
 
 
 if __name__ == "__main__":
