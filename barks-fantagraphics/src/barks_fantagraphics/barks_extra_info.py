47b82a85ea00d47c83ac682be169bc1320fe264a
glk1001 <gregg.kay@gmail.com>
2025-05-11 20:51:37 +1000

    Added some more 'extra info'.

--- barks-fantagraphics/src/barks_fantagraphics/barks_extra_info.py
+++ barks-fantagraphics/src/barks_fantagraphics/barks_extra_info.py
@@ -2,11 +2,16 @@ from barks_fantagraphics.barks_titles import Titles
 
 BARKS_EXTRA_INFO = {
     Titles.GOOD_NEIGHBORS: "First appearance of [b][i]Neighbor Jones.[/i][/b]",
+    Titles.CHRISTMAS_ON_BEAR_MOUNTAIN: "First appearance of [b][i]Uncle Scrooge.[/i][/b]",
     Titles.LOST_IN_THE_ANDES: 'In a 1962 interview, Barks agreed that "my best story, technically,'
     ' is probably the square egg one."\n\n'
     "From 'www.seriesam.com': \"Western Publishing objected to the accent Barks had given the"
     " Plain Awful people and so they (for example) replaced incorrect use of 'd' with 'th'"
-    ' in their dialogue." So in this digital edition, the Southern accents have been fixed to what'
-    " Barks originally intended, using [i]Dis, Dat, Dey, D', De,[/i] and [i]Dere.[/i]"
-    " (Pages 20-24, 26-29, and 31, reconstructed by GLK, April 2025)",
+    ' in their dialogue." Accordingly, in this digital edition, the Southern accents have been'
+    " restored to what Barks originally intended, using [i]Dis, Dat, Dey, D', De,[/i] and"
+    " [i]Dere.[/i] (Pages 20-24, 26-29, and 31, reconstructed by GLK, April 2025)",
+    Titles.WINTERTIME_WAGER: "First appearance of [b][i]Gladstone Gander.[/i][/b]",
+    Titles.OPERATION_ST_BERNARD: "First appearance of [b][i]The Junior Woodchucks.[/i][/b]",
+    Titles.TERROR_OF_THE_BEAGLE_BOYS: "First appearance of [b][i]The Beagle Boys.[/i][/b]",
+    Titles.BIG_BIN_ON_KILLMOTOR_HILL_THE: "First appearance of the [b][i]Money Bin.[/i][/b]",
 }
