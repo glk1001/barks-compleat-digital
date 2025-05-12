d2b3523fd79982ea480dc1ba63c8813da0f96bba
glk1001 <gregg.kay@gmail.com>
2025-05-12 23:23:48 +1000

    More 'extra info'.

--- barks-fantagraphics/src/barks_fantagraphics/barks_extra_info.py
+++ barks-fantagraphics/src/barks_fantagraphics/barks_extra_info.py
@@ -1,7 +1,16 @@
 from barks_fantagraphics.barks_titles import Titles
 
 BARKS_EXTRA_INFO = {
-    Titles.GOOD_NEIGHBORS: "First appearance of [b][i]Neighbor Jones.[/i][/b]",
+    Titles.MIGHTY_TRAPPER_THE: "First appearance of [b][i]Daisy.[/i][/b].",
+    Titles.GOOD_DEEDS: "First appearance of [b][i]Neighbor Jones.[/i][/b]."
+    " (Mr. Jones in this story.)",
+    Titles.GOOD_NEIGHBORS: "The first real appearance of [b][i]Neighbor Jones[/i][/b]"
+    " - the one we all know!",
+    Titles.THREE_DIRTY_LITTLE_DUCKS: "First appearance of [b][i]Herbert[/i][/b]"
+    " and [b][i]Bornworthy[/i][/b] (aka [b][i]Bolivar)[/i][/b] the dog.",
+    Titles.MAD_CHEMIST_THE: "First appearance of [b][i]Car 313.[/i][/b]",
+    Titles.HIGH_WIRE_DAREDEVILS: "First mention of [b][i]Duckburg.[/i][/b]"
+    " (Last panel of last page.)",
     Titles.CHRISTMAS_ON_BEAR_MOUNTAIN: "First appearance of [b][i]Uncle Scrooge.[/i][/b]",
     Titles.LOST_IN_THE_ANDES: 'In a 1962 interview, Barks agreed that "my best story, technically,'
     ' is probably the square egg one."\n\n'
@@ -12,6 +21,12 @@ BARKS_EXTRA_INFO = {
     " [i]Dere.[/i] (Pages 20-24, 26-29, and 31, reconstructed by GLK, April 2025)",
     Titles.WINTERTIME_WAGER: "First appearance of [b][i]Gladstone Gander.[/i][/b]",
     Titles.OPERATION_ST_BERNARD: "First appearance of [b][i]The Junior Woodchucks.[/i][/b]",
-    Titles.TERROR_OF_THE_BEAGLE_BOYS: "First appearance of [b][i]The Beagle Boys.[/i][/b]",
+    Titles.PIXILATED_PARROT_THE: "First appearance of what looks like the [b][i]Money Bin[/i][/b]"
+    " but in this story is called the Safe.",
+    Titles.TERROR_OF_THE_BEAGLE_BOYS: "First appearance of [b][i]The Beagle Boys.[/i][/b]"
+    " And first mention of the [b][i]Money Bin.[/i][/b]",
     Titles.BIG_BIN_ON_KILLMOTOR_HILL_THE: "First appearance of the [b][i]Money Bin.[/i][/b]",
+    Titles.ROUND_MONEY_BIN_THE: "First appearance of [b][i]Number One Dime.[/i][/b]",
+    Titles.CAT_BOX_THE: "First appearance of [b][i]Little Helper.[/i][/b]",
+    Titles.MIDAS_TOUCH_THE: "First appearance of [b][i]Magica de Spell.[/i][/b]",
 }
