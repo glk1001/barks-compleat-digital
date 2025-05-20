4b7af5e60b3931976a3b769c321b033e118a3bb2
glk1001 <gregg.kay@gmail.com>
2025-05-20 21:01:13 +1000

    Added more extra info.

--- barks-fantagraphics/src/barks_fantagraphics/barks_extra_info.py
+++ barks-fantagraphics/src/barks_fantagraphics/barks_extra_info.py
@@ -1,6 +1,22 @@
 from barks_fantagraphics.barks_titles import Titles
 
 BARKS_EXTRA_INFO = {
+    Titles.VICTORY_GARDEN_THE: " This was Barks's first comic-book work after leaving the Disney"
+    " studio."  
+    " \n\nFrom Michael Barrier:"
+    " There is in the Disney Archives an undated note to Barks from the"
+    ' Disney studio, signed "Dorothy" (probably Dorothy Strebe, who worked in the Disney'
+    ' publications department for more than twenty years), that reads in part as follows: [i]"Here'
+    " is a 10-page story for Donald Duck. Hope that you like it ... you are to stage it, of course"
+    " ... and if you see that it can be strengthened, or that it deviates from Donald either in"
+    ' narration or action, please make the improvements."[/i]',
+    Titles.RABBITS_FOOT_THE: "The first Donald Duck story both written and drawn by Barks."
+    "\n\nFrom a letter from Barks to Michael Barrier:"
+    '[i]"They sent me a ready-written script to draw for story #1 several weeks after I left the'
+    " studio... The script was the Crow story [WDCS 31]. I noticed some bad errors in the plot, and"
+    " wrote the editor for permission to change things around. The result was they invited me to"
+    " see if I could do a whole original story myself. The story of the kids and their"
+    " 'rabbit foot' was my answer to the opportunity.\"[/i]",
     Titles.TOO_MANY_PETS: "In a June 9, 1968 letter to Michael Barrier, Barks wrote:"
     " [i]\"In the Sept. 1943 D.D. the story 'Too Many Pets' was a sort of collaboration. The script"
     " was started by a writer at the Disney Studio who found himself so overloaded with work on"
@@ -18,19 +34,20 @@ BARKS_EXTRA_INFO = {
     Titles.GOOD_NEIGHBORS: "The first real appearance of [b][i]Neighbor Jones[/i][/b]"
     " - the one we all know!",
     Titles.THREE_DIRTY_LITTLE_DUCKS: "First appearance of [b][i]Herbert[/i][/b]"
-    " and [b][i]Bornworthy[/i][/b] (aka [b][i]Bolivar)[/i][/b] the dog.",
+    " and [b][i]Bornworthy[/i][/b] (aka [b][i]Bolivar)[/i][/b] the dog."
+    "\n\nAnd the first Barks Duck story drawn in a four tier layout.",
     Titles.MAD_CHEMIST_THE: "First appearance of [b][i]Car 313.[/i][/b]",
     Titles.HIGH_WIRE_DAREDEVILS: "First mention of [b][i]Duckburg.[/i][/b]"
     " (Last panel of last page.)",
     Titles.CHRISTMAS_ON_BEAR_MOUNTAIN: "First appearance of [b][i]Uncle Scrooge.[/i][/b]",
     Titles.LOST_IN_THE_ANDES: 'In a 1962 interview, Barks agreed that [i]"my best story,'
     ' technically, is probably the square egg one."[/i]\n\n'
-    "From 'www.seriesam.com': [i]\"Western Publishing objected to the accent Barks had given the"
+    "From 'www.seriesam.com:' [i]\"Western Publishing objected to the accent Barks had given the"
     " Plain Awful people and so they (for example) replaced incorrect use of 'd' with 'th'"
     ' in their dialogue."[/i] Accordingly, in this digital edition, the Southern accents have been'
     " restored to what Barks originally intended, using [i]Dis, Dat, Dey, D', De,[/i] and"
     " [i]Dere.[/i] (Pages 20-24, 26-29, and 31, reconstructed by GLK, April 2025)",
-    Titles.VOODOO_HOODOO: 'From \'www.seriesam.com\': [i]"Barks originally drew \'Bombie the'
+    Titles.VOODOO_HOODOO: "From 'www.seriesam.com:' [i]\"Barks originally drew 'Bombie the"
     " Zombie' with blank eyeballs. The editors at Western Publishing feared that the vacant eyes"
     ' might frighten children and so they added pupils and half-closed lids."[/i] In this digital'
     " edition, Bombie has been changed back to having blank eyeballs.",
