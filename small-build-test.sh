17016cde80a1f7074d9a57e6fd91d86e648676e8
glk1001 <gregg.kay@gmail.com>
2025-06-25 13:26:13 +1000

    Added small build test script.

---
+++ small-build-test.sh
@@ -0,0 +1,9 @@
+set -e
+
+python build-comics/batch-build-comics.py build --title "Good Neighbors"
+python build-comics/batch-build-comics.py build --title "Mystery of the Swamp"
+python build-comics/batch-build-comics.py build --title "Donald Duck's Best Christmas"
+python build-comics/batch-build-comics.py build --title "The Bill Collectors"
+python build-comics/batch-build-comics.py build --title "Donald Duck's Atom Bomb"
+python build-comics/batch-build-comics.py build --title "Lost in the Andes!"
+python build-comics/batch-build-comics.py build --title "Only a Poor Old Man"
